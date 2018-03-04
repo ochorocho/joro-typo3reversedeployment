ada7dbdea6afdb322af13cab3294807e92f6e45f
ochorocho <rothjochen@gmail.com>
2018-03-04 14:30:06 +0100

    Make sure all variables are in place

--- JoRo/Typo3ReverseDeployment.php
+++ JoRo/Typo3ReverseDeployment.php
@@ -7,6 +7,11 @@ Class Typo3ReverseDeployment {
     /**
      * @var string
      */
+    protected $user = "root";
+
+    /**
+     * @var string
+     */
     protected $pubKey = "~/.ssh/id_rsa";
 
     /**
@@ -21,7 +26,7 @@ Class Typo3ReverseDeployment {
      *
      * @var string
      */
-    protected $sqlTarget = ".data/db/db-dump";
+    protected $sqlTarget = "./tmp/";
 
     /**
      * Tables to exclude during export
@@ -29,6 +34,22 @@ Class Typo3ReverseDeployment {
      * @var array
      */
     protected $sqlExcludeTable = ["sys_log","sys_history","cf_cache_hash","cf_cache_hash_tags","cf_extbase_datamapfactory_datamap","cf_extbase_datamapfactory_datamap_tags","cf_extbase_object","cf_extbase_object_tags","cf_extbase_reflection","cf_extbase_reflection_tags"];
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
 
     /**
      * @return string
@@ -105,15 +126,15 @@ Class Typo3ReverseDeployment {
      * @param $user
      * @return \phpseclib\Net\SSH2
      */
-    public function ssh($host, $user) {
+    public function ssh($host) {
         $key = new \phpseclib\Crypt\RSA();
         $key->loadKey(file_get_contents($this->getPubKey()));
 
         $ssh = new \phpseclib\Net\SSH2($host);
-        if (!$ssh->login($user, $key)) {
+        if (!$ssh->login($this->getUser(), $key)) {
             exit("\033[31mLogin Failed\033[0m" . PHP_EOL);
         } else {
-            echo "\033[32mSSH to $user@$host successful\033[0m" . PHP_EOL;
+            echo "\033[32mSSH to ".$this->getUser()."@$host successful\033[0m" . PHP_EOL;
         }
         return $ssh;
     }
@@ -141,25 +162,24 @@ Class Typo3ReverseDeployment {
         $conf = $this->getLocalConfiguration($ssh);
 
         /**
-         * Build --ignore-table option
+         * Build --exclude-tables for `typo3cms database:export` command
          */
-        $ignoredTables = count($this->sqlExcludeTable) > 0 ? " --ignore-table={" : "";
+        $ignoredTables = count($this->sqlExcludeTable) > 0 ? " --exclude-tables " : "";
         foreach ($this->sqlExcludeTable as $exclude) {
-            if(end($this->sqlExcludeTable) == $exclude) {
-                $space = "";
-            } else {
-                $space = ",";
-            }
-            $ignoredTables .= $conf['dbname']. "." .$exclude . $space;
+            $ignoredTables .= end($this->sqlExcludeTable) == $exclude ? $exclude . '' : $exclude . ',';
         }
-        $ignoredTables .= count($this->sqlExcludeTable) > 0 ? "}" : "";;
 
         /**
          * Export and download database
          */
-        $dbRemoteTarget = $this->getTypo3RootPath() . 'typo3temp/' . date("Ymds") . "-" . $conf['dbname'] . ".sql";
-        $ssh->exec("mysqldump " . $conf['dbname'] ." > " . $dbRemoteTarget . " -u" . $conf['user'] . " -p" . $conf['password'] . " -h" . $conf['host'] . $ignoredTables);
-        exec("rsync -avz jochen@knallimall.org:$dbRemoteTarget ./tmp/");
-        return $dbRemoteTarget . PHP_EOL;
+        $sqlRemoteTarget = $this->getTypo3RootPath() . 'typo3temp/' . date("Ymds") . "-" . $conf['dbname'] . ".sql";
+        $sqlExport = "cd " . $this->getTypo3RootPath() . " && ../vendor/bin/typo3cms database:export";
+
+        echo "\033[32mExport DB: $sqlExport\033[0m" . PHP_EOL;
+        $ssh->exec($sqlExport . " $ignoredTables > $sqlRemoteTarget");
+        exec("rsync -avz ".$this->getUser()."@$ssh->host:$sqlRemoteTarget " . $this->getSqlTarget());
+        $ssh->exec("rm -f $sqlRemoteTarget");
+
+        return $sqlRemoteTarget;
     }
 }
