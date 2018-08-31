b19b7113d7691b20b1e598f15d4da77d117899fc
Albrecht Köhnlein <ak@koehnlein.eu>
2018-08-31 21:02:21 +0200

    Add very simple unit test

---
+++ Tests/Unit/JoRo/Typo3ReverseDeploymentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JoRo\Tests\Unit;
+
+use JoRo\Typo3ReverseDeployment;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class Typo3ReverseDeploymentTest extends TestCase
+{
+    /**
+     * @var Typo3ReverseDeployment
+     */
+    protected $subject;
+
+    public function setUp() {
+        $this->subject = new Typo3ReverseDeployment;
+    }
+
+    /**
+     * @test
+     */
+    public function sqlExcludeTableSetterAndGetter() {
+        // set value
+        $testArray = ['foo', 'bar'];
+        $this->subject->setSqlExcludeTable($testArray);
+        // test getter
+        $this->assertEquals($testArray, $this->subject->getSqlExcludeTable());
+    }
+}
