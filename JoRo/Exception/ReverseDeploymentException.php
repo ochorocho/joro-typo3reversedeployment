dcf183195ea8eb0af2eff21c12bb1257a4a54dae
André Schließer <a.schliesser@zeroseven.de>
2020-03-20 14:46:22 +0100

    Refactor code and simplify commands

---
+++ JoRo/Exception/ReverseDeploymentException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JoRo\Exception;
+
+/***
+ *
+ * This file is part of the "Contactmap" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2020 zeroseven design studios GmbH
+ * André Schließer <a.schliesser@zeroseven.de>
+ *
+ ***/
+
+/**
+ * Class Exception
+ * @package JoRo\Exception
+ */
+class ReverseDeploymentException extends \Exception
+{
+}
